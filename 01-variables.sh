3261156c8452d24f5c23152a8660c652bb7a9e7b
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 00:17:59 +0530

    example-1

---
+++ 01-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Ram
+PERSON2=Prem
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I'm good. How are you?"
+echo "$PERSON1: I'm doing great $PERSON2, What day is today?"
+echo "$PERSON2: Very Good morning $PERSON1, Today is Sunday, it's holiday"
+
+echo "This is an example of using a simple variable in the script."
