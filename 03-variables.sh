4949cda236bc14d72f0b5bbd4afccb7d9eb832a3
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 00:18:29 +0530

    example-3

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Taking a command output into a variable
+
+DATE1=$(date +%F)
+DATE2=$(date +%H-%M-%S)
+
+echo "Today's date is $DATE1, and the script started executing at $DATE2."
+
+echo "This is an example of run the command inside a scipt and store into a variable"
