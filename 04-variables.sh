0ac28c9d6d9522ced5b0e0915b714093ed922b21
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 00:18:41 +0530

    example-4

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Taking a command output into a variable
+
+echo "enter your name: "
+read name
+
+echo "What colour do you like ?"
+read -s colour
+
+echo "$name likes $colour colour."
+
