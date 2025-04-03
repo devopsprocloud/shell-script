b7cdf59c43d93007a11bd5f1eec1695d7fd2396f
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 08:41:26 +0530

    example-2

---
+++ 12-install-git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [$ID -ne 0 ]
+then 
+    echo "You are not a 'root' user"
+    exit 1
+else
+    dnf install gitt -y 
+    if  [ $? -ne 0 ]
+    then 
+        echo "Installing Git is FAILED."
+    else 
+        echo "Installing GIT is SUCCESS."
+    fi
+fi
