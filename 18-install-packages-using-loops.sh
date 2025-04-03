b1ec376b7002dfeb8495b7da4a0201fcf4a98563
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 23:38:47 +0530

    example-1

--- 18-install-packages-using-loops.sh
+++ 18-install-packages-using-loops.sh
@@ -7,6 +7,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date -%F+%H-%H-%S)
+LOGFILE=/tmp/$@-$TIMESTAMP.log
+
+echo -e "$Y Script started executing at $TIMESTAMP $N."
+
 VALIDATE() {
     if  [ $1 -ne 0 ]
     then 
@@ -24,13 +29,13 @@ then
 else
     for package in $@ 
     do 
-        yum list installed $package
-        if [ $? -ne 0 ]
+        yum list installed $package &>> $LOGFILE
+        if [ $? -ne 0 ] # If not installed
         then
-            yum install $package -y
-            VALIDATE $? "Installing $package"
+            yum install $package -y &>> $LOGFILE # install the package
+            VALIDATE $? "Installing $package" # validate 
         else
-            echo -e "$package is already installed...$Y SKIPPING $N."
+            echo -e "$package is already installed...$Y SKIPPING $N." # If installed already give the output
         fi
     done
 fi
