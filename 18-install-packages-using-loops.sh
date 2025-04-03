dd0f362af123e7e36cdc643ec659ed11a5b5e1a5
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 23:30:54 +0530

    example-1

--- 18-install-packages-using-loops.sh
+++ 18-install-packages-using-loops.sh
@@ -10,7 +10,7 @@ N="\e[0m"
 VALIDATE() {
     if  [ $1 -ne 0 ]
     then 
-        echo -e "$1...$R FAILED $N."
+        echo -e "$2...$R FAILED $N."
         exit 1
     else
         echo -e "$2...$G SUCCESS $N"
