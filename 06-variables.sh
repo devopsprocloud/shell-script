01a0d376ccedbf992f76c1cd2069629b75e344ce
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-03 07:32:37 +0530

    example-6

---
+++ 06-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Calculating sum of given two numbers
+
+echo "enter first number:"
+read num1
+
+echo "enter second number:"
+read num2
+
+SUM=$(($num1 + $num2))
+
+echo "Total value of given two numbers is $SUM."
+
+
