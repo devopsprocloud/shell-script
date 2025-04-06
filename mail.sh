fcf0032afc7bb92a5d24584d943d6b3b2ea7e732
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 14:18:59 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -9,7 +9,7 @@ TO_ADDRESS=$5
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" mail-template.html)
 
-#echo "$FINAL_BODY" | s-nail -s "$SUBJECT" "$TO_ADDRESS"
+echo "$FINAL_BODY" | s-nail -s "$SUBJECT" "$TO_ADDRESS"
 
 #echo -e "$FINAL_BODY\nContent-Type: text/html" | s-nail -s "$SUBJECT" "$TO_ADDRESS"
 
@@ -21,4 +21,4 @@ FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s
 #printf "%s\nContent-Type: text/html\n" "$SUBJECT" | s-nail -s "$FINAL_BODY" "$TO_ADDRESS"
 
 
-echo "From: $FROM_ADDRESS\nTo: $TO_ADDRESS\nSubject: $SUBJECT\nMime-Version: 1.0\nContent-Type: text/html\nContent-Transfer-Encoding: 8bit\n\n$(cat mail-template.html)" | s-nail -s "$SUBJECT" $TO_ADDRESS"
+#echo "From: $FROM_ADDRESS\nTo: $TO_ADDRESS\nSubject: $SUBJECT\nMime-Version: 1.0\nContent-Type: text/html\nContent-Transfer-Encoding: 8bit\n\n$(cat mail-template.html)" | s-nail -s "$SUBJECT" $TO_ADDRESS"
