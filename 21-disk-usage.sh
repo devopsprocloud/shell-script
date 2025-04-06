3e9f092ab176ba6bdd9813d8d2eb8c16f9c82a0c
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 09:51:50 +0530

    example-1

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -3,10 +3,10 @@
 THRESHOLD=1
 DISK_USAGE=$(df -hT | grep xvd)
 message=""
-USAGE_PERCENTAGE=$(df -hT | grep xvd | awk -F  '{print $6F}' | cut -d % -f1)
-DISK_VOLUMES=$(df -hT | grep xvd | awk -F  '{print $1F}')
+USAGE_PERCENTAGE=$(df -hT | grep xvd | awk '{print $6F}' | cut -d % -f1)
+DISK_VOLUMES=$(df -hT | grep xvd | awk '{print $1F}')
 
-while IFS
+while IFS= read line
 do 
     if [ $USAGE_PERCENTAGE -gt 1 ]  
     then 
