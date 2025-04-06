935e751e7b619de33e8a0f681d1ee7a9c5906d21
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 09:20:22 +0530

    example-1

--- 20-ifs.sh
+++ 20-ifs.sh
@@ -12,9 +12,10 @@ then
     echo -e "$R The file $file does not exist $N"
 fi
 
-while IFS=":" read username passwd shell
+while IFS=":" read username passwd user_id group_id user_fullname home_dir shell
 do
     echo -e "username is: $G $username $N"
     echo -e "password is: $G $passwd $N"
-    echo -e "shell is: $G $shell $N"
+    echo -e "full name is: $G $user_fullname $N"
+    echo -e "group ID is : $G $group_id $N"
 done < $file
