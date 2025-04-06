e18726957892519b03cc1c480b31f07b467c5672
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 14:01:46 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -11,7 +11,7 @@ FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s
 
 #echo "$FINAL_BODY" | s-nail -s "$SUBJECT" "$TO_ADDRESS"
 
-"(echo -e "$FINAL_BODY\nContent-Type: text/html")" | s-nail -s "$SUBJECT" "$TO_ADDRESS"
+echo -e "$FINAL_BODY\nContent-Type: text/html" | s-nail -s "$SUBJECT" "$TO_ADDRESS"
 
 #echo "$FINAL_BODY" | s-nail -s "$(printf "%s\nContent-Type: text/html" "$SUBJECT")" "$TO_ADDRESS"
 
