cdb95342de65fa60f5573afd8e8f1868fdca00ea
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 13:15:15 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TO_ADDRESS=$1
+TO_TEAM=$1
 SUBJECT=$2
 ALERT_TYPE=$3
 BODY=$4
-ESCAPED_BODY=
-FROM_ADDRESS=$5
+ESCAPED_BODY=""
+TO_ADDRESS=$5
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" mail-template.html)
 
