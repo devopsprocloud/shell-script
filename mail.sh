db908a6824e557c986ccdaf7e4a62efd04daad53
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 13:21:20 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -4,7 +4,7 @@ TO_TEAM=$1
 SUBJECT=$2
 ALERT_TYPE=$3
 BODY=$4
-ESCAPED_BODY=""
+ESCAPED_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$5
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" mail-template.html)
