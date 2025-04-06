acf63da0cebf47a494d3aa79987e36c8b5fd9d1b
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 08:18:17 +0530

    example-1

--- 19-delete-old-logs.sh
+++ 19-delete-old-logs.sh
@@ -8,10 +8,10 @@ read source_dir
 if [ ! -d $source_dir ]
 then
     echo "Source directory $source_dir does not exist."
-else
-    while IFS= read -r line 
-    do 
-        echo "Deleting $line "
-        rm -rf $line
-    done <<< $FILES_TO_DELETE
 fi
+
+while IFS= read -r line 
+do 
+    echo "Deleting $line "
+    rm -rf $line
+done <<< $FILES_TO_DELETE
